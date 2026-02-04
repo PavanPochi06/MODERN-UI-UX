39380bcaf0f96b0fd4bdbc8ff86a116580c70fae
PavanPochi06 <pavanpochi06@gmail.com>
2026-02-04 16:04:19 +0530

    enhance Features page with pricing section

--- Webify/src/components/Pricing.jsx
+++ Webify/src/components/Pricing.jsx
@@ -1,3 +1,95 @@
-export default function Pricing(){
-  return <div></div>
+import { Star } from "lucide-react";
+
+const plans = [
+  {
+    name: "Starter",
+    price: "29.99",
+    description: "Perfect for individuals and small teams",
+    features: [
+      "Up to 5 teams members",
+      "10GB storage",
+      "Basic analytics",
+      "Email support",
+      "API access",
+      "Mobile app",
+    ],
+    mostPopular: false,
+  },
+  {
+    name: "professinal",
+    price: "79.99",
+    description: "Best for growing business",
+    features: [
+      "Up to 5 teams members",
+      "100GB storage",
+      "Advanced analytics",
+      "Priority support",
+      "API access",
+      "Mobile app",
+      "Advanced security",
+    ],
+    mostPopular: true,
+  },
+  {
+    name: "Enterprise",
+    price: "199.99",
+    description: "For large organigations",
+    features: [
+      "Unlimited team members",
+      "unlimited storage",
+      "Enterprise analytics",
+      "24/7 pphone support",
+      "API access",
+      "Mobile app",
+      "Custom integration",
+      "Advandced security",
+      "Dedicated account manager",
+      "Custom SLA",
+    ],
+    mostPopular: false,
+  },
+];
+
+export default function Pricing() {
+  return (
+    <section
+      id="pricing"
+      className="py-16 sm:py-20 px-10 sm:px-6 lg:px-8 relative"
+    >
+      <div className="max-w-7xl mx:auto">
+        <div className="text-center mb-12 sm:mb-16 lg:mb-20">
+          <h2 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 sm:mb-6">
+            <span className="bg-gradient-to-b from-white to-gray-300 bg-clip-text text-transparent">
+              Simple, Transparent
+            </span>
+            <br />
+            <span className="bg-gradient-to-b from-blue-400 to-cyan-400 bg-clip-text text-transparent">
+              Pricing Plans
+            </span>
+          </h2>
+          <p className="text-gray-400 text-base text-xl sm:text-lg sm:text-lg max-2-2xl ma-auto">
+            Choose the perfect plan your needs. All plans include a 14-day free
+            trail.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-6">
+          {plans.map((plan, key) => (
+            <div
+              key={key}
+              className={`relative bg-slate-900 backdrop-blur-sm border rounded-xl sm:rounded-2xl p-6 sm:p-8 transition-all duration-300 overflow-visible group flex flex-col h-full ${plan.mostPopular ? "border-blue-500 shadow-2xl shadow-blue-500/20 lg:scale-105" : "border-slate-800 hover:border-slate-700"}`}
+            >
+              {plan.mostPopular && (
+                <div className="absolute -top-2 sm:-top-3 left-1/2 transform -translate-x-1/2 z-10">
+                  <div className="flex items-center space-x-1 px-3 sm:py-1.5 bg-gradient-to-b from-blue-500 to-cyan-500 rounded-full text-xs sm:text-sm font-semibold shadow-lg">
+                    <Star className="w-3 h-3 sm:h-3 fill-white" />
+                    <span>Most Popular</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
