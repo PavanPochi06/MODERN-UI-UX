7ca0d8a0ef3b2aa419cea8ef7871b4f2540ec95e
PavanPochi06 <pavanpochi06@gmail.com>
2026-02-04 16:49:29 +0530

    improve pricing layout in Pricing.jsx

--- Webify/src/components/Pricing.jsx
+++ Webify/src/components/Pricing.jsx
@@ -1,4 +1,4 @@
-import { Star } from "lucide-react";
+import { Check, Star } from "lucide-react";
 
 const plans = [
   {
@@ -16,7 +16,7 @@ const plans = [
     mostPopular: false,
   },
   {
-    name: "professinal",
+    name: "Professinal",
     price: "79.99",
     description: "Best for growing business",
     features: [
@@ -86,8 +86,44 @@ export default function Pricing() {
                   </div>
                 </div>
               )}
+
+              <div className="text-center mb-6 sm:mb-8">
+                <h3 className="text-xl sm:text-2xl font-bold mb-2">
+                  {plan.name}
+                </h3>
+                <p className="text-gray-400 text-xs sm:text-sm mb-3 sm:mb-4">
+                  {plan.description}
+                </p>
+                <div className="flex items-baseline justify-center">
+                  <span className="text-3xl sm:text-4xl lg:text-5xl font-bold bg-gradient-to-r from-blue-400 to-cyan-400 bg-clip-text text-transparent">{plan.price}</span>
+                  <span className="text-gray-400 ml-1 sm:ml-2 text-sm sm:text-base">/monthly</span>
+                </div>
+              </div>
+              <ul className="space-y-3 sm:space-y-4 mb-6 sm:mb-8 flex-row">
+                {plan.features.map((feature, featureKey) => (
+                  <li key={featureKey} className="flex items-start space-x-2 sm:space-x-3">
+                    <div className="flex-shrink w-4 h-4 sm:w-5 sm:h-5 rounded-full bg-blue-500/20 flex items-center justify-center mt-0.5">
+                      <Check className="w-2.5 h-2.5 sm:h-3 sm:w-3 text-blue-400" />
+                    </div>
+                    <span className="text-gray-300 text-sm sm:text-base">
+                      {feature}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+              <button className={`w-full py-2.5 sm:py-3 px-4 sm:px-6 rounded-lg font-semibold transition-all duration-300 mt-auto hover:scale-102 cursor-pointer text-sm sm:text-base ${plan.mostPopular
+                ? "bg-gradient-to-b from-blue-500 to-cyan-500"
+                : "bg-white/5 border border-white/10 hover:bg-white/10"
+              }`}>
+                Get Started
+              </button>
             </div>
           ))}
+        </div>
+        <div className="mt-8 sm:mt-12 text-center">
+          <p className="text-gray-400 text-base text-xl">
+            Need a custom plan? <a href="#" className="text-blue-400 hover:text-blue-300">Contact our sales team</a>
+          </p>
         </div>
       </div>
     </section>
